8f0536079c4456042e2ce62c591d659cb57c7ed4
Chris Ballance <chris.ballance@physics.ox.ac.uk>
2017-02-06 22:56:06 +0000

    Added a more flexible plot_xy

---
+++ artiqApplets/plot_xy.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3.5
+# From Artiq, modified to have optional line-style
+
+import numpy as np
+import PyQt5  # make sure pyqtgraph imports Qt5
+import pyqtgraph
+
+from artiq.applets.simple import TitleApplet
+
+
+class XYPlot(pyqtgraph.PlotWidget):
+    def __init__(self, args):
+        pyqtgraph.PlotWidget.__init__(self)
+        self.args = args
+
+    def data_changed(self, data, mods, title):
+        try:
+            y = data[self.args.y][1]
+        except KeyError:
+            return
+        x = np.array(data.get(self.args.x, (False, None))[1])
+        if x is None:
+            x = np.arange(len(y))
+        error = data.get(self.args.error, (False, None))[1]
+        fit = data.get(self.args.fit, (False, None))[1]
+
+        if not len(y) or len(y) != len(x):
+            return
+        if error is not None and hasattr(error, "__len__"):
+            if not len(error):
+                error = None
+            elif len(error) != len(y):
+                return
+        if fit is not None:
+            if not len(fit):
+                fit = None
+            elif len(fit) != len(y):
+                return
+
+        xi = np.argsort(x)
+
+        if self.args.xscale:
+            x /= self.args.xscale
+
+        self.clear()
+        symbol = "x" if not self.args.no_symbol else None
+        pen = pyqtgraph.mkPen() if self.args.lines else None
+        self.plot(x[xi], y[xi], pen=pen, symbol=symbol)
+        self.setTitle(title)
+        if error is not None:
+            # See https://github.com/pyqtgraph/pyqtgraph/issues/211
+            if hasattr(error, "__len__") and not isinstance(error, np.ndarray):
+                error = np.array(error)
+            errbars = pyqtgraph.ErrorBarItem(
+                x=np.array(x), y=np.array(y), height=error)
+            self.addItem(errbars)
+        if fit is not None:
+            self.plot(x[xi], fit[xi], pen=pyqtgraph.mkPen('g'))
+
+        if self.args.xlabel:
+            self.setLabel("bottom", self.args.xlabel)
+        if self.args.ylabel:
+            self.setLabel("left", self.args.ylabel)
+
+
+def main():
+    applet = TitleApplet(XYPlot)
+    applet.add_dataset("y", "Y values")
+    applet.add_dataset("x", "X values", required=False)
+    applet.add_dataset("error", "Error bars for each X value", required=False)
+    applet.add_dataset("fit", "Fit values for each X value", required=False)
+
+    applet.argparser.add_argument("--lines", action="store_true",
+                                  help="Join data points with lines")
+    applet.argparser.add_argument("--no-symbol", action="store_true",
+                                  help="Do not use a data-point symbol")
+    applet.argparser.add_argument("--xlabel", help="Label for x axis")
+    applet.argparser.add_argument("--ylabel", help="Label for y axis")
+    applet.argparser.add_argument("--xscale", type=float, 
+                                  help="Scale x axis values")
+    applet.run()
+
+if __name__ == "__main__":
+    main()
